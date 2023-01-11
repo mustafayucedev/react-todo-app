d339febf6d0d4bbb1e5e2955b0be103fa9386ac2
Mustafa Yüce <75587873+mustafayucedev@users.noreply.github.com>
2023-01-11 22:24:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState , useEffect } from "react";
+import { useState } from "react";
 import "App.css";
 import Header from 'components/Header/Index';
 import Form from 'components/Form/Index';
@@ -6,14 +6,9 @@ import List from 'components/List/Index';
 
 function App() {
 
-  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
   const [input,setInput] = useState("");
-  const [todos,setTodos] = useState([initialState]);
+  const [todos,setTodos] = useState([]);
   const [editTodo,setEditTodo] = useState(null);
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
 
   return (
     <div className="container">
